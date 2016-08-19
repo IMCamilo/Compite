a0600c54566d990f575a6f49a13e687c3154ae26
pofo99@gmail.com <pofo99@gmail.com>
2016-08-18 20:07:52 -0400

    trate de hacer los controladores de transporte como me acordaba,  aun esta en prgreso

--- grails-app/controllers/compite/TransporteController.groovy
+++ grails-app/controllers/compite/TransporteController.groovy
@@ -6,27 +6,49 @@ package compite
 class TransporteController {
 
     def index = {
+
     }
 
     def create = {
+        def transporte = Transporte.list()
+        [transporte:transporte]
+
     }
 
     def save = {
+        def transporte = new Transporte(params)
+        transporte.save flush:true , failOnError:true
+        redirect action: show , id:transporte.id
     }
 
     def edit = {
+        def transporte = Transporte.get(params.id)
+        [transporte: transporte]
     }
 
     def update = {
+        def transporte = new Transporte(params)
+        transporte.properties = params
+        transporte.save flush:true , failOnError:true
+        redirect action: show , id:transporte.id
     }
 
     def show = {
+        def transporte = Transporte.get(params.id)
+        [transporte: transporte]
+
     }
 
     def list = {
+        def transporte = Transporte.list()
+        [transporte:transporte]
     }
 
     def delete = {
+        def transporte = new Transporte(params)
+        transporte.properties = params
+        transporte.delete flush:true , failOnError:true
+        redirect action: show , id:transporte.id
     }
 
 }
