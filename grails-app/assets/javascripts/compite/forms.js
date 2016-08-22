bc18486aa0cf26d53ee00af992835a8e4cabc3de
cepardov <cepardov@gmail.com>
2016-08-22 15:35:34 -0300

    Front-end: Página de error con materialize

--- grails-app/assets/javascripts/compite/forms.js
+++ grails-app/assets/javascripts/compite/forms.js
@@ -17,11 +17,4 @@ jQuery(document).ready(function ($) {
 
 		$('select').material_select();
 	});
-	$('.button-collapse').sideNav({
-			//menuWidth: 300, // Default is 240
-			//edge: 'right', // Choose the horizontal origin
-			//closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		}
-	);
-	// Show sideNav
 });
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -42,7 +42,6 @@
         </nav>
     </div>
 </nav>
-<div id="load"></div>
 <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
         <div class="container">
