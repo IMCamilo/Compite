98c3e8dc4d3f3257deddc7344b1e3e1e15c82998
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2016-08-29 12:39:59 -0300

    Seleccion asignacion proyectos por usuario

--- grails-app/controllers/compite/IngenieroController.groovy
+++ grails-app/controllers/compite/IngenieroController.groovy
@@ -1,13 +1,27 @@
 package compite
 
+
 class IngenieroController {
+
+    private BigInteger usuarioId = session.usuarioLogueado.id
 
     def index = {
         redirect(action: "proyectos")
     }
 
     def proyectos = {
-        //crear una vista para seleccionar el proyecto en base el usuario logeado
+        println "estos son los proyectos del usuario "+usuarioId
+        //def gg = Asignacion.findAll("from Asignacion as b where b.detalle=?", ['asignacion 1, cargada automaticamente'])
+        def asignaciones = Asignacion.findAll("from Asignacion as b where b.usuario="+usuarioId)
+        println "===>"
+        println "id : "+ asignaciones.id[0]
+        println "version : "+ asignaciones.version[0]
+        println "detalle : "+ asignaciones.detalle[0]
+        println "fecha : "+ asignaciones.fecha[0]
+        println "proyecto : "+ asignaciones.proyecto[0]
+        println "usuario : "+ asignaciones.usuario[0]
+        //http://docs.grails.org/3.1.1/ref/Domain%20Classes/executeQuery.html
+        //en la busqueda en proyectos, pasar una lista, ya que de asignaciones pueden venir muchos id.
     }
 
     def nuevarendicion = {
