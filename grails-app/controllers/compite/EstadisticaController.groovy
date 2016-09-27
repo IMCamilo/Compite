e3edf58a258c14de9644db16869c144c5975f562
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2016-09-27 11:39:44 -0300

    Actualizacion charts

--- grails-app/controllers/compite/EstadisticaController.groovy
+++ grails-app/controllers/compite/EstadisticaController.groovy
@@ -3,13 +3,25 @@ package compite
 class EstadisticaController {
 
     def index() {
-        //cantidad proyectos publicos y privados
+
         def ttlPub = Proyecto.countByTipo('PUBLICO')
         def ttlPriv = Proyecto.countByTipo('PRIVADO')
         def qtyProyectos = ttlPub + ttlPriv
         def percentPub = (ttlPub/qtyProyectos)*100
         def percentPri = (ttlPriv/qtyProyectos)*100
-        [percentPub:percentPub,percentPri:percentPri]
+
+        def audAprobada = Auditoria.countByEstado('APROBADA')
+        def audRechazada = Auditoria.countByEstado('RECHAZADA')
+        def qtyAuditorias = audRechazada + audAprobada
+        def auditoriasAprobadas = (audAprobada/qtyAuditorias)*100
+        def auditoriasRechazadas = (audRechazada/qtyAuditorias)*100
+
+        [
+            percentPub:percentPub,
+            percentPri:percentPri,
+            auditoriasAprobadas:auditoriasAprobadas,
+            auditoriasRechazadas:auditoriasRechazadas
+        ]
     }
 
 }
